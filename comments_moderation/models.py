403e65e31e50b4f0824384375b65da1f061b4b6b
Petr Dlouhý <petr.dlouhy@email.cz>
2013-10-16 18:30:08 +0200

    models description in english

--- comments_moderation/models.py
+++ comments_moderation/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.utils.translation import ugettext as _
 
 class EmailFilter(models.Model):
-    email  = models.EmailField(verbose_name=u"Blokovaný email", null=True)
-    active = models.BooleanField(help_text=u"Pravidlo je aktivní")
+    email  = models.EmailField(verbose_name=_(u"Email"), null=True)
+    active = models.BooleanField(help_text=_(u"Rule is active"))
